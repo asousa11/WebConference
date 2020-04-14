d3392b6d1f715f03d4838d31007405cbe71d8ea5
asousa16 <andre.f.c.sousa@hotmail.com>
2020-04-14 20:48:49 +0000

    Section 'Oradores' complete. Need to remove HTML from Oradores 'div'.

--- js/webconf.js
+++ js/webconf.js
@@ -43,3 +43,63 @@ btnRegister.addEventListener("click", function() {
         }
     })
 })
+
+( async () => {
+    const renderSpeakers = document.getElementById("renderSpeakers")
+    let txtSpeakers = ""
+    const response = await fetch('${url_Base}/conference/1/speakers')
+    const speakers = await response.json()
+
+    for (const speaker of speakers) {
+        txtSpeakers += `
+            <div class="col-sm-4">
+                <div class="team-member">
+                    <!--Orador nº1-->
+                    <img id="${speaker.idSpeaker}" class="mx-auto rounded-circle viewSpeaker" src="img/team/1.jpg" alt="Primeiro Orador">
+                    <h4>André Baptista</h4>
+                    <p class="text-muted">Security Engineer</p>
+                    <ul class="list-inline social-buttons">`
+        if (speaker.twitter !== null) {
+            txtSpeakers.txtSpeakers += `
+                <li class="list-inline-item">
+                    <a href="#"><i class="fab fa-twitter"></i></a>
+                </li>`
+        }
+        if (speaker.facebook !== null) {
+            txtSpeakers.txtSpeakers += `
+                <li class="list-inline-item">
+                    <a href="#"><i class="fab fa-facebook.f"></i></a>
+                </li>`
+        }
+        if (speaker.linkedin !== null) {
+            txtSpeakers.txtSpeakers += `
+                <li class="list-inline-item">
+                    <a href="#"><i class="fab fa-linkedin-in"></i></a>
+                </li>`
+        }
+        txtSpeakers += `
+                    </ul>
+                </div>
+            </div>`
+    }
+    const btnView = document.getElementsByClassName("viewSpeaker")
+    for (let i=0; i < btnView.length; i++) {
+        btnView[i].addEventListener("click", () => {
+            for (const speaker of speakers) {
+                if (speaker.idSpeaker == btnView[i].getAttribute("id")) {
+                    // janela modal
+                    Swal.fire({
+                        title: speaker.nome,
+                        text: speaker.bio,
+                        imageUrl: speaker.foto,
+                        imageWidth: 400,
+                        imageHeight: 400,
+                        imageAlt: 'Foto do Orador',
+                        animation: false
+                    })
+                }
+            }
+        })
+    }
+    renderSpeakers.innerHTML = txtSpeakers
+}) ()
